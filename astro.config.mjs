3ac2ebda2819528aed590f2ed72955d3d641e6eb
Jhamil Taborga <jhamiltaborga@gmail.com>
2025-08-28 19:34:13 -0400

    Agregamos el componente donde usaremos las Server Islands y agregamos un adaptador para poder utilizar las Server Islands en Astro, en este caso utilizamos el de Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,25 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
+  // Agregamos el "output: 'server'," lo que hará esto es crear un servidor que solo se activara 
+  // cuando lo necesite y así lo que sea estático seguirá estático pero lo que se necesite ejecutar
+  // en un servidor se ejecutará en un servidor.
+  output: 'server',
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   env: {
     schema: {
       SHOW_BUY_BUTTON: envField.boolean({ default:true, context: 'server', access: 'public' }),
       SCORE_API_ENDPOINT: envField.string({ context:'server', access:'public' }),
     }
-  }
+  },
+
+  adapter: vercel()
 });
